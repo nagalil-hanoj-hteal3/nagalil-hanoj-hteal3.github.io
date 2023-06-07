0c60a504438dd27210204cea65e0d2b6abe705b0
nagalil-honaj-hteal3 <96222174+nagalil-honaj-hteal3@users.noreply.github.com>
2023-06-07 15:09:25 -0700

    home menu saves

--- js/index.js
+++ js/index.js
@@ -1,15 +1,33 @@
-// JavaScript code to create auto-typing effect
 const textElement = document.querySelector('.typing-text');
 const text = "Welcome to my website. Pick any of the following below to learn more about me.";
+
 let index = 0;
 
 function type() {
-    if (index < text.length) {
-        textElement.textContent += text.charAt(index);
-        index++;
-        setTimeout(type, 50); // Adjust typing speed (milliseconds)
+    let substring = text.substring(0, index);
+    textElement.textContent = substring;
+    index++;
+    if (index <= text.length) {
+        setTimeout(type, 35); // Adjust typing speed (milliseconds)
+    } else {
+        // Start typing the date
+        const textElem = document.querySelector('.date-type');
+        const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+        const today = new Date().toLocaleDateString('en-US', options);
+        let index2 = 0;
+
+        function type2() {
+            if (index2 < today.length) {
+                textElem.textContent += today.charAt(index2);
+                index2++;
+                setTimeout(type2, 50); // Adjust typing speed (milliseconds)
+            }
+        }
+
+        // Start the date auto-typing effect
+        type2();
     }
 }
 
-// Start the auto-typing effect
+// Start the first auto-typing effect
 type();
