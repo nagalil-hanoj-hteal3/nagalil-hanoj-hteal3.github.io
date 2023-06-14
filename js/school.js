4839f724c6617e96d9f7346d040f3b008ef2e4af
nagalil-honaj-hteal3 <96222174+nagalil-honaj-hteal3@users.noreply.github.com>
2023-06-13 23:05:52 -0700

    changes added for my education. needs more

--- js/school.js
+++ js/school.js
@@ -124,7 +124,25 @@ const textContent2 = "Hey there! This is where my career started as a software e
 "about. It would make my experience much more unbalanced as I would have to learn how to sacrifice and learn how to set my priorities. Additionally, it would force me "+
 "to adjust my focus when I had to pay attention to school or with training. As this would be the final semester for online courses, this would become a difficult task "+
 "for me to know how to set my schedule for future semesters.\n\n<b>Sophomore Year:</b>\n<u>Fall Semester</u>\nBIOL-104 -> Human Biology\nCS-231 -> Assembly Language "+
-"& Digital Circuits\nCS-311 -> Data Structures & Algorithms\nMATH-270 -> Basic Discrete Math\n\nReflection: ";
+"& Digital Circuits\nCS-311 -> Data Structures & Algorithms\nMATH-270 -> Basic Discrete Math\n\nReflection: Transitioning back to in person training and classes would "+
+"be rough and unprecedented as it has been two years that I have went back to school in person. Although my anxiety of coming back kicks in, I was also excited to see "+
+"and experience the life of a college student and to meet new people. Given that this would be a rough semester with adjusting my schedule, it would continue to be my "+
+"challenge that I would have to build and sooner or later would be simple for me to handle.\n\n<u>Spring Semester</u>\nCS-351 -> Programming Languages\nMATH-242 -> "+
+"Introduction to Statistics\nPHIL-110 -> Critical Thinking\nPHYS-101 -> Introduction to Physics I\nSE-370 -> Introduction to Software Engineering\n\nReflection: Entering "+
+"upper division courses as well as adding in general ed requirements. Not only was that a challenge, but involving training during draft selection would be crucial. "+
+"Unfortunately, it would not be possible for me to balance between training and school at the same time due to commute and time conflicts. It would then force me to "+
+"focus myself with academics rather than prioritizing training as well to keep myself competitive in draft selection. From here, this was where I chose to pursue my education."+
+"\n\n<b>Junior Year:</b>\n<u>Fall Semester</u>\nCS-331 -> Computer Architecture\nCS-433 -> Operating Systems\nCS-443 -> Database Systems\nMATH-264 -> Introduction to Linear "+
+"Algebra\nSE-451 -> Software Requirements and Design\n\nReflection: With my first semester out of ROTC, this sparked a new journey to explore my options with my education "+
+"career. Going heavy into my software engineering major to learn which types of computer classes that interests me. For the meantime, I decided to take CS courses that would "+
+"be prerequisites for other courses to see if I can obtain that same interest into the future courses. As far as I can say, it was easy to say that the CS courses that I "+
+"would take are mainly software instead of hardware due to its flexibility and mantainability.\n\n<u>Spring Semester</u>\nCIS-444 -> Web Programming\nCS-446 -> Cloud Computing\n"+
+"GEOG-201 -> World Regional Geography\nSE-461 -> Software Testing & Quality\nSE-471 -> Software Architecture\n\nReflection: By far one of the funnest semesters I have been a part of."+
+" Not only would this be a crucial semester for me to obtain practices into future courses, but one of my successful semesters yet in college. Some projects I did here this semester "+
+"would be applied into my resume and experiences for me to display for interviews and colleagues to practice more of my software engineering principles into projects. This would also "+
+"be my first semester I started working as a grader and a tutor, which expanded my knowledge of how to apply more knowledge from other peers to understand concepts in different ways.\n\n"+
+"<u>Summer Semester</u>\nPHYS-102 -> Introduction to Physics II\n\nReflection: \n\n<b>Senior Year:</b>\n<u>Fall Semester</u>\nCS-481 -> Intro to Mobile Programming\nPHIL-348 -> Ethics "+
+"in Engineering\nSE-481 -> Software Project Plan & Management\nSE-490 -> Senior Project I\n\nReflection: ";
 
 let skip2 = false; // Flag variable to track if skip button is clicked
 
@@ -156,36 +174,109 @@ type2();
 
 //===========================================================================================================
 
+const textElement3 = document.querySelector('.sdsu-text');
+const skipButton3 = document.querySelector('.skip-button3');
+
+const textContent3 = "Standby. Area-tenchut!\n\nAt ease. Ladies & Gentlemen, this is where my training career has been established. First off, let's begin how I was "+
+"a part of this detachment.\n\nFun story, I began this program three weeks when it first started since I was not offically enrolled into SDSU as I am a student in CSUSM. "+
+"Since I was not 'enrolled' into campus, I had to get the process complete to be in SDSU, however, since this was during COVID, this would allow me to start training.\n\n"+
+"<b>Freshmen Year:</b> Start of IMT (Initial Military Training) or Introductory Training for ROTC.\n\n<u>Fall Semester</u>\nLeadership Labratory (LLAB)\nAS-100-A\nNovember"+
+"Flight\nRank: C/4C\n\nReflection: Being apart of a leadership program for the first time would be a very interesting position as there are people with different perspective of "+
+"how to approach situations. At times it can be a success or failure depending on the team as some people can get along versus others. Learning how to swallow my pride was the "+
+"one thing I had to do for not preventing any friction and sour in a team. Given that this was online, it would lack on the in person experience of controlling the flow and "+
+"not having that in person camaraderie of meeting the other cadets in person would be the drawback of this experience.\n\n<u>Spring Semester</u>\nLeadership Labratory (LLAB)\n"+
+"AS-100-B\nMike Flight\nRank: C/4C\n\nReflection: With a new semester means a new team and new adjustments. While this would still be an online experience, same expectations "+
+"were still applied here. ";
+
+let skip3 = false; // Flag variable to track if skip button is clicked
+
+function type3() {
+    let substring = textContent3.substring(0, index3);
+    textElement3.innerHTML = substring;
+    index3++;
+    if (index3 <= textContent3.length && !skip3) {
+      setTimeout(type3, 25); // Adjust typing speed (milliseconds)
+    } else {
+      skipButton3.style.display = 'none';
+    }
+}
+
+// Function to skip the typing animation
+function skipTyping3() {
+    skip3 = true;
+    textElement3.innerHTML = textContent3; // Set the entire text content
+    index3 = textContent3.length; // Set the index to the end of the text content
+    skipButton3.style.display = 'none';
+}
+
+// Attach event listener to the skip button
+skipButton3.addEventListener('click', skipTyping3);
+
+// Start the auto-typing effect
+let index3 = 0;
+type3();
+
+//===========================================================================================================
+
+const textElement4 = document.querySelector('.swc-text');
+const skipButton4 = document.querySelector('.skip-button4');
+
+const textContent4 = "What's up!";
+
+let skip4 = false; // Flag variable to track if skip button is clicked
+
+function type4() {
+    let substring = textContent4.substring(0, index4);
+    textElement4.innerHTML = substring;
+    index4++;
+    if (index4 <= textContent3.length && !skip4) {
+      setTimeout(type3, 25); // Adjust typing speed (milliseconds)
+    } else {
+      skipButton4.style.display = 'none';
+    }
+}
+
+// Function to skip the typing animation
+function skipTyping4() {
+    skip4 = true;
+    textElement4.innerHTML = textContent4; // Set the entire text content
+    index4 = textContent4.length; // Set the index to the end of the text content
+    skipButton4.style.display = 'none';
+}
+
+// Attach event listener to the skip button
+skipButton4.addEventListener('click', skipTyping4);
+
+// Start the auto-typing effect
+let index4 = 0;
+type4();
+
+//====================================================================================================================
+
 const cardWrapper = document.querySelector('.card-wrapper');
 const prevButton = document.querySelector('.prev-button');
 const nextButton = document.querySelector('.next-button');
 
 window.addEventListener('scroll', function() {
-// Check if the user has scrolled enough to show the buttons
-    if (window.scrollY > 50) {
-        prevButton.style.opacity = '1';
-        nextButton.style.opacity = '1';
-    } else {
-        prevButton.style.opacity = '0';
-        nextButton.style.opacity = '0';
-    }
+  // Check if the user has scrolled enough to show the buttons
+  if (window.scrollY > 50) {
+    prevButton.style.opacity = '1';
+    nextButton.style.opacity = '1';
+  } else {
+    prevButton.style.opacity = '0';
+    nextButton.style.opacity = '0';
+  }
 });
 
 const cards = Array.from(cardWrapper.getElementsByClassName('card'));
 let currentIndex = 0;
 
 prevButton.addEventListener('click', () => {
-  if (currentIndex > 0) {
-    currentIndex--;
-    updateCardVisibility();
-  }
+  goToPreviousCard();
 });
 
 nextButton.addEventListener('click', () => {
-  if (currentIndex < cards.length - 1) {
-    currentIndex++;
-    updateCardVisibility();
-  }
+  goToNextCard();
 });
 
 function updateCardVisibility() {
@@ -198,7 +289,26 @@ function updateCardVisibility() {
   });
 }
 
+function updateIndex() {
+  if (currentIndex < 0) {
+    currentIndex = cards.length - 1;
+  } else if (currentIndex >= cards.length) {
+    currentIndex = 0;
+  }
+}
+
+function goToPreviousCard() {
+  currentIndex--;
+  updateIndex();
+  updateCardVisibility();
+}
+
+function goToNextCard() {
+  currentIndex++;
+  updateIndex();
+  updateCardVisibility();
+}
+
 // Show the initial active card
 updateCardVisibility();
 
-//=====================================================================================================
